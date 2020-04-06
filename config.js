fdf318dd2ae461c2736cacb850b0d93aabf70170
James Bithell <james@bithell.com>
2020-04-06 17:46:30 +0100

    First draft

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "title": "Bithell Studios",
+  "name": "bithell_studios",
+  "description": "Bithell Studios Status Page",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-GB",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "json",
+	"systems": [
+		"adamrms-api", "adamrms-site", "adamrms-notifications", "adamrms-app", "beepos-login", "beepos-endofday"
+	]
+  },
+  "theme": {
+		"links": {
+			"contact": "https://studios.jbithell.com/",
+			"home": "https://studios.jbithell.com/"
+		},
+  },
+  "baseUrl": "https://status.jbithell.com",
+}
---
+++ content/2020-04-06_adamrms-homepage-not-displaying-correctly.md
@@ -0,0 +1,14 @@
+---json
+{
+  "title": "AdamRMS Homepage not displaying correctly",
+  "date": "2020-04-06T15:12:27.347Z",
+  "severity": "partial-outage",
+  "affectedsystems": [
+    "site-delivery"
+  ],
+  "resolved": false
+}
+---
+Project view displaying instead of homepage
+
+<!--- language code: en -->
